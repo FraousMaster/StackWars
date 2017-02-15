138ee69049700e19a52b9ec01ad0aa08d28f8fc3
fhugo <hugo.frost@live.se>
2017-02-15 18:35:49 +0100

    Created GameState

---
+++ src/GameState.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class GameState {
+
+    private ArrayList<Ant> ants;
+    public GameState(){
+        ants = new ArrayList<Ant>();
+        initGame();
+    }
+
+    public void initGame(){
+        for(int i = 0; i <= 0; i++){
+            ants.add(new Ant(i * 5, i * 2, Ant.player.PlayerFour));
+        }
+    }
+
+    public ArrayList<Ant> getUppdates(){
+        return ants;
+    }
+
+    public void uppdateGameState(ArrayList<Ant> a){
+        ants = a;
+    }
+
+}
