1a7029c2811383d050e70fb5bc37d17e1144a3f0
fhugo <hugo.frost@live.se>
2017-02-23 14:13:53 +0100

    test

---
+++ src/Client.java
@@ -0,0 +1,106 @@
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedList;
+public class Client extends Thread{
+	private static String IP_ADDRESS;
+	private static String name;
+	private static byte[] sendData = new byte[16];
+	private static byte[] receiveData = new byte[16];
+	private DatagramSocket clientSocket;
+	private String SendMessage;
+	private LobbyMenu menu;
+	private String messageReceived;
+	private LinkedList<String> players ;
+	private boolean gameGo = true;
+	private ArrayList<Ant> ants;
+	private GameState state;
+	
+	@SuppressWarnings("static-access")
+	public Client(String IP, String name, LobbyMenu menu) throws SocketException{
+		this.IP_ADDRESS = IP;
+		this.name = name;
+		this.menu = menu;
+		players = new LinkedList<String>();
+		clientSocket = new DatagramSocket();	
+		state.initGame();
+	}
+	
+	 public void run(){
+		 InetAddress host;
+		
+			try {
+				
+				host = InetAddress.getByName(IP_ADDRESS);
+				
+				clientSocket.setSoTimeout(500);
+				SendMessage = name;
+				sendData = SendMessage.getBytes();
+				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, host, 1203);
+			 	clientSocket.send(sendPacket);
+				System.out.println("This was sent from clientsocket: " + SendMessage);
+		
+				try{
+				while(true){
+				
+				DatagramPacket receivethis = new DatagramPacket(receiveData, receiveData.length);
+				clientSocket.receive(receivethis);
+				byte[] data = receivethis.getData();
+				messageReceived = new String(data, 0, receivethis.getLength());
+				new Player(messageReceived).start();
+						}
+				} catch (SocketTimeoutException e) {
+					System.out.println("socket timeout");
+					multicastInit();
+				    }
+				} catch (SocketTimeoutException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+		e.printStackTrace();
+	}
+	 }
+
+	 private void multicastInit() throws UnknownHostException{
+		 DatagramPacket receivePacket = new DatagramPacket(receiveData,receiveData.length);
+		 String INET_ADDR = "230.0.0.1";
+		 InetAddress address;
+	
+		try {
+			address = InetAddress.getByName(INET_ADDR);
+
+			 MulticastSocket multiSocket = new MulticastSocket(8888);
+			 multiSocket.joinGroup(address);
+			 while(gameGo){
+				 	receiveData = receivePacket.getData();
+				 	multiSocket.receive(receivePacket);
+					messageReceived = new String(receiveData, 0 ,receivePacket.getLength() );
+
+					System.out.println("This was received from multi: " + messageReceived);	
+					new Player(messageReceived).run();
+			 }
+		}catch (IOException e) {
+			e.printStackTrace();
+		} 
+	 }
+ 
+	public class Player extends Thread {
+		String name;
+		
+		public Player(String player){
+			this.name = player;
+		    players.add(name);
+		    System.out.println("entered player : " + name);
+		    System.out.println("linkedlist in client : " + players);
+		    }
+		
+		public void run() 
+		{
+			menu.setPlayers(name);
+
+
+		}
+		
+	}
+}
+	
---
+++ src/GameMenu.java
@@ -0,0 +1,223 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.UnknownHostException;
+
+import javax.swing.*;
+
+public class GameMenu extends JPanel {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JTextField ipField;
+	private JTextField nameField;
+	@SuppressWarnings("rawtypes")
+	private JComboBox antBox;
+	private JButton mapButton;
+	private JButton gameButton;
+	private JButton backButton;
+	private JButton joinButton;
+	private String IP_ADDRESS;
+	private String NAME;
+	private String antNr;
+	private final String[] ants = {"Ants","5","6","7","8","9","10"};
+
+
+	
+	public GameMenu(int to){
+		setLayout(new GridBagLayout());
+		if(to == 0){
+		hostSettings();
+		}
+		else{
+		joinSettings();
+		
+		}
+	}
+
+	public String getName(){
+		return NAME;
+	}
+	
+	private void joinSettings(){
+		GridBagConstraints c = new GridBagConstraints();
+		ipField = new JTextField("Ip Address");
+		nameField = new JTextField("Name");
+		joinButton = new JButton("Join");
+		backButton = new JButton("Back");
+		
+		ipField.addActionListener(new Handler());
+		nameField.addActionListener(new Handler());
+		joinButton.addActionListener(new Handler());
+		backButton.addActionListener(new Handler());
+		
+		ipField.setPreferredSize(new Dimension(100, 50));
+		nameField.setPreferredSize(new Dimension(100, 50));
+		joinButton.setPreferredSize(new Dimension(100, 50));
+		backButton.setPreferredSize(new Dimension(100, 50));
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 3;
+		c.gridy = 0;
+		add(ipField, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 3;
+		c.gridy = 1;
+		add(nameField, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 3;
+		c.gridy = 2;
+		add(joinButton, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 3;
+		c.gridy = 3;
+		add(backButton, c);
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private void hostSettings(){
+		
+		
+		GridBagConstraints c = new GridBagConstraints();
+		
+		//ipField = new JTextField("Ip Address");
+		nameField = new JTextField("Name");
+		antBox = new JComboBox(ants);
+		mapButton = new JButton("Map");
+		gameButton = new JButton("Create Game");
+		backButton = new JButton("Back");
+		
+		antBox.addActionListener(new Handler());
+		mapButton.addActionListener(new Handler());
+		gameButton.addActionListener(new Handler());
+		backButton.addActionListener(new Handler());
+		
+		//ipField.setPreferredSize(new Dimension(100, 50));
+		nameField.setPreferredSize(new Dimension(100, 50));
+		antBox.setPreferredSize(new Dimension(100, 50));
+		mapButton.setPreferredSize(new Dimension(100, 50));
+		gameButton.setPreferredSize(new Dimension(150, 50));
+		backButton.setPreferredSize(new Dimension(100, 50));
+
+		
+		//c.fill = GridBagConstraints.HORIZONTAL;
+		//c.gridx = 3;
+		//c.gridy = 0;
+		//add(ipField, c);
+	
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 3;
+		c.gridy = 1;
+		add(nameField, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 3;
+		c.gridy = 2;
+		add(antBox, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 3;
+		c.gridy = 3;
+		add(mapButton, c);
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 3;
+		c.gridy = 4;
+		add(gameButton, c);
+	
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 3;
+		c.gridy = 5;
+		add(backButton, c);
+	}
+	
+	private void setIP(String input){
+		this.IP_ADDRESS = input;
+	}
+	
+	public String returnIP(){
+		return IP_ADDRESS;
+	}
+	
+	private void returnName(String input){
+		this.NAME = input;
+		
+	}
+	
+	private void returnAnts(String input){
+		this.antNr = input;
+	}
+	
+	public void remove(){
+		this.removeAll();
+		this.revalidate();
+		this.repaint();
+	}
+	
+	private void createHost() throws UnknownHostException{
+		LobbyMenu menu = new LobbyMenu(0, this );
+		add(menu);
+		
+	}
+	
+    private void createClient() throws UnknownHostException{
+    	LobbyMenu menu = new LobbyMenu(1, this );	
+		add(menu);
+		
+	}
+	
+	public class Handler implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == backButton){
+				remove();
+				PlayMenu menu = new PlayMenu();
+				add(menu);
+			}
+			else if(e.getSource() == antBox ){
+				String value = antBox.getSelectedItem().toString();
+				returnAnts(value);	
+			}	
+			else if(e.getSource() == gameButton ){
+				String input2 = nameField.getText();
+				returnName(input2);
+				remove();
+				try {
+					createHost();
+				} catch (UnknownHostException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}	
+			else if(e.getSource() == joinButton ){
+				String input2 = nameField.getText();
+				returnName(input2);
+				String input = ipField.getText();
+				setIP(input);
+				remove();
+				try {
+					createClient();
+				} catch (UnknownHostException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+				
+				
+		
+			
+			
+		}
+	}
+}
+
+
+	
+
