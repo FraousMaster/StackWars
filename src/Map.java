54cfcfd347208aa67bd9de1c8f848a289fbfc803
fhugo <hugo.frost@live.se>
2017-03-01 19:31:34 +0100

    small change to map

--- src/Map.java
+++ src/Map.java
@@ -37,7 +37,7 @@ public class Map {
         FileReader fr;
         int i, x = 0, y = 0;
             try {
-                fr = new FileReader("Graphics\\Maps\\Map2.txt");
+                fr = new FileReader("Graphics\\Maps\\Map1.txt");
 
                 while ((i = fr.read()) != -1)
                 {
