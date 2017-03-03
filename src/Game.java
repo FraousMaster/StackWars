2b4de64027d61322682c2ac009dca19b92d23535
arvidwi <arvidwi@student.chalmers.se>
2017-03-03 20:26:17 +0100

    asd

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,13 @@
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import Global.Resources;
 
 public class Game {
@@ -5,6 +15,7 @@ public class Game {
     private GameController gameController = null;
     private GameState gameState = null;
     public Game() {
+    	readingSetting();
         gameState = new GameState();
         gameView = new GameView(gameState);
         gameController = new GameController(gameView, gameState);
@@ -16,5 +27,25 @@ public class Game {
     public GameState getState(){
     	return gameState;
     	
+    }
+    
+    private void readingSettings(){
+        try{
+            File settFile = new File("../Settings/file.xml");
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(settFile);
+            doc.getDocumentElement().normalize();
+            System.out.println("Root Element:" + doc.getDocumentElement().getNodeName());
+            NodeList nodeList = doc.getElementsByTagName("Game");
+                Node node = nodeList.item(0);
+                Element eElement = (Element) node;
+                String temp[];
+                String name = eElement.getElementsByTagName("resolution").item(0).getTextContent();
+                temp = name.split("x");
+                System.out.println("pls: " + Integer.parseInt(temp[0]));
+        }catch(Exception filee){
+            filee.printStackTrace();
+        }
     }
 }
--- src/Global/Resources.java
+++ src/Global/Resources.java
@@ -38,24 +38,4 @@ public class Resources {
 	{
 		width = y;
 	}
-	
-	private void readingSettings(){
-        try{
-            File settFile = new File("../Settings/file.xml");
-            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse(settFile);
-            doc.getDocumentElement().normalize();
-            System.out.println("Root Element:" + doc.getDocumentElement().getNodeName());
-            NodeList nodeList = doc.getElementsByTagName("Game");
-                Node node = nodeList.item(0);
-                Element eElement = (Element) node;
-                String temp[];
-                String name = eElement.getElementsByTagName("resolution").item(0).getTextContent();
-                temp = name.split("x");
-                System.out.println("pls: " + Integer.parseInt(temp[0]));
-        }catch(Exception filee){
-            filee.printStackTrace();
-        }
-    }
 }
