b79359db1babf804e1e5ba5349a73166fcdb7a18
Johannes <edenholm@student.chalmers.se>
2017-03-04 23:51:34 +0100

    sry hugo

--- src/Credits.java
+++ src/Credits.java
@@ -1,11 +1,19 @@
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import Global.Resources;
 
 public class Credits extends JPanel {
+	
+	private JButton start;
+	private JButton back;
+	
+	
 
     public Credits(){
     	setLayout(new GridBagLayout());
@@ -17,6 +25,15 @@ public class Credits extends JPanel {
     	JLabel z = new JLabel("are ");
     	JLabel a = new JLabel("awesome");
     	
+    	
+    	start = new JButton("Start");
+		back = new JButton("Back");
+		start.addActionListener(new Handler());
+		back.addActionListener(new Handler());
+		
+		add(start);
+		add(back);
+    	
     	add(x);
     	add(z);
     	add(a);
@@ -24,5 +41,28 @@ public class Credits extends JPanel {
     	
     	
     }
+    
+	public void remove(){
+		this.removeAll();
+		this.revalidate();
+		this.repaint();
+	}
+    
+    public class Handler implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == start){
+				new Game();
+			}
+		
+			else if(e.getSource() == back){
+					remove();
+					StartMenu menu = new StartMenu();
+					add(menu);
+				}
+			}
+		}
+    
+    
     
 }
