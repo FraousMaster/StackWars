3a9567518aecfd63ae4fc8edb5a686dea0d78a7e
fhugo <hugo.frost@live.se>
2017-03-05 18:00:10 +0100

    added things

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.io.File;
+import javax.swing.*;
+import javax.sound.sampled.*;
 
 public class Main {
 	public static void main(final String[] args){
 		new Menu();
+		File file =	new File("Sound/CC Red Alert 3 Theme - Soviet March.wav");
+		try {
+            Clip clip = AudioSystem.getClip();
+            // getAudioInputStream() also accepts a File or InputStream
+            AudioInputStream ais = AudioSystem.
+                    getAudioInputStream(file);
+            clip.open(ais);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        }catch (Exception e){
+		    e.printStackTrace();
+        }
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+			}
+		});
 	}
 }
