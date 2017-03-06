7e00d310b96feb686a20ac4ace0f511cf3f1b45d
fhugo <hugo.frost@live.se>
2017-03-06 14:38:46 +0100

    added sprites

--- src/GameView.java
+++ src/GameView.java
@@ -103,7 +103,7 @@ public class GameView extends Observable implements Observer{
 				try {
 
                 	if(sprite <= 2) {
-						image = ImageIO.read(new File("Graphics/Ant/smallAnt.png"));
+						image = ImageIO.read(new File("Graphics/Ant/AntV1.png"));
 						sprite++;
 					}
 					else if(sprite <= 4){
